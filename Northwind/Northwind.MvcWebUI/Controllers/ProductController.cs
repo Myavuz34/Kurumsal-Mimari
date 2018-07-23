ee9270de7a97a17d21e66961602b48a0ec9f553e
Mustafa YAVUZ <myavuz53@gmail.com>
2018-07-23 22:21:04 +0300

    ProductManager değişti.

--- Northwind/Northwind.MvcWebUI/Controllers/ProductController.cs
+++ Northwind/Northwind.MvcWebUI/Controllers/ProductController.cs
@@ -6,16 +6,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Northwind.Entities;
+using Northwind.Interfaces;
 
 namespace Northwind.MvcWebUI.Controllers
 {
     public class ProductController : Controller
     {
+        private IProductService _productService;
 
-        ProductManager _productManager = new ProductManager(new EfProductDal());
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public  ActionResult Index()
         {
-            List<Product> products = _productManager.GetAll();
+            List<Product> products = _productService.GetAll();
             return View(products);
         }
     }
